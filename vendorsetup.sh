6fd6f37964067893cee42cede5d874d80995a6bf
iykequame <izaqkull@yahoo.com>
2018-06-23 01:07:17 +0000

    use "git apply -v" instead for ease ..

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,10 +7,10 @@ echo
 echo "  :( "
 echo "Applying PATCHES ..."
 cd system/core
-patch -p1 < ../../device/motorola/woods/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+git apply -v ../../device/motorola/woods/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 cd ../..
 cd bionic
-patch -p1 < ../device/motorola/woods/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
+git apply -v ../device/motorola/woods/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
 cd ..
 
 echo ""
