f182df427da9c76ebce2819e8117cacbffecacc6
iykequame <izaqkull@yahoo.com>
2018-06-24 23:25:34 +0000

    remove -all

--- patches/apply-patches.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ../../../..
-cd system/core
-patch -p1 < ../../device/motorola/woods/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-cd ../..
-cd bionic
-patch -p1 < ../device/motorola/woods/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
-cd ..
-
