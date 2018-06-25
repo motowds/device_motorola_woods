bfe05889be5e1f5ba78181cc2dd5e97c62d48057
iykequame <izaqkull@yahoo.com>
2018-06-25 01:31:26 +0000

    woods: update Gatekeeper src dir

--- gatekeeper/Android.mk
+++ gatekeeper/Android.mk
@@ -4,8 +4,8 @@
 #
 # =============================================================================
 
-#LOCAL_PATH := $(call my-dir)
-LOCAL_PATH := device/motorola/woods
+LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := device/motorola/woods
 
 include $(CLEAR_VARS)
 
